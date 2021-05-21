d845fb4171ce238983bcbea322b28e6cd2697314
Derek <dewen@cca.edu>
2021-05-21 15:29:02 -0700

    L298N test

--- I2CTest/Ringside/Ringside.ino
+++ I2CTest/Ringside/Ringside.ino
@@ -3,7 +3,7 @@
 #include <Wire.h>
 
 // Pix Ring
-#define NEOPIX_PIN 11
+#define NEOPIX_PIN 12
 #define STRIPSIZE 12
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIPSIZE, NEOPIX_PIN, NEO_GRBW + NEO_KHZ800);
 
@@ -11,7 +11,13 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIPSIZE, NEOPIX_PIN, NEO_GRBW + NE
 HT16K33 seg(0x70);
 int numDis = 0;
 
-#define DCMOTOR_PIN 10
+// DC Motors
+#define L298N_IN1 3
+#define L298N_IN2 4
+#define L298N_IN3 5
+#define L298N_IN4 6
+#define L298N_ENA 9
+#define L298N_ENB 10
 
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
@@ -19,8 +25,13 @@ void setup() {
   // Pix Ring
   strip.begin();
   strip.setBrightness(10);
-  // Motor placeholder
-  pinMode(DCMOTOR_PIN, OUTPUT);
+  // Motor
+  pinMode(L298N_IN1, OUTPUT);
+  pinMode(L298N_IN2, OUTPUT);
+  pinMode(L298N_IN3, OUTPUT);
+  pinMode(L298N_IN4, OUTPUT);
+  pinMode(L298N_ENA, OUTPUT);
+  pinMode(L298N_ENA, OUTPUT);
   // 7-seg
   seg.begin();
   Wire.setClock(100000);
@@ -82,9 +93,6 @@ void loop() {
 
   // 1 Dc motor
   if(c[1]) {
-    digitalWrite(DCMOTOR_PIN, HIGH);
-  } else {
-    digitalWrite(DCMOTOR_PIN, LOW);
   }
 
   // 2 7-seg
@@ -95,9 +103,17 @@ void loop() {
     } else {
       numDis = numDis*10+c[2];
     }   
-    Serial.println(numDis);
+    // Serial.println(numDis);
   }
   seg.displayInt(numDis);
+
+  // DC Motors
+  analogWrite(L298N_ENA, 255); // don't go under 50
+  analogWrite(L298N_ENB, 255);
+  digitalWrite(L298N_IN1, HIGH);
+  digitalWrite(L298N_IN2, LOW);
+  digitalWrite(L298N_IN3, LOW);
+  digitalWrite(L298N_IN4, HIGH);
   
   delay(100);
 }
